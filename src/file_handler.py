69b3db3eb28e24a689d3b58b11d7b89a59bbcf48
AhmedAmrAwadallahK8 <awadallahahmed25@gmail.com>
2023-07-03 17:36:11 -0700

    Commented file_handler.py

--- src/file_handler.py
+++ src/file_handler.py
@@ -3,6 +3,17 @@ import cv2 as cv
 supported_img_files = ["jpg", "png"]
 
 def process_img_type(img, ext):
+    """
+    Converts supported imgs into the expected format for this program which is
+    a HxWxC where the C axis is of length 3
+
+    Args:
+        img (ndarray): numpy array containing data related to an image
+        ext (str): extension associated with the loaded image
+
+    Returns:
+        ndarray: numpy array containing the image in expected format
+    """
     if ext == "jpg":
         return img
     elif ext == "png":
@@ -10,6 +21,16 @@ def process_img_type(img, ext):
         return img
 
 def load_img_file():
+    """
+    Prompts user for image file, extracts the image, processes it, and then
+    returns it
+
+    Args:
+        Nothing
+
+    Returns:
+        ndarray: numpy array containing the image in expected format
+    """
     img_path = input("Specify Img File Path: ")
     img = cv.imread(img_path, cv.IMREAD_UNCHANGED)
     img_extension = img_path.split(".")[-1]
